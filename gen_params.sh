b48ad9f1f19b054aa91c0a4d7ee4a817f7c39795
Alessio Marzullo <alessio.marzullo@txtgroup.com>
2024-07-09 16:31:38 +0200

    metodo per scelta numero targa

--- gen_params.sh
+++ gen_params.sh
@@ -5,8 +5,62 @@
 # usage menu
 echo
 echo "---------------------- Usage ----------------------"
-echo -e "\n   bash $0\n\n    -n <number of triples to generate> \n    -f <first_plate_chars> (ex. AB) \n    -l <last_plate_chars> (ex. CD) \n"
+echo -e "\n   bash $0\n\n    -n < number of triples to generate > \n    -p < plate number chars > (ex. ABCD) \n    -o < optional, plate number prg >"
 echo
+
+# to do  param opzionale in cui vado ad inserire io la partenza della targa 
+
+while getopts n:p:o: flag
+do
+    	case "${flag}" in
+		n) n=${OPTARG};;
+        p) PLATE_NUMBER=${OPTARG};;
+        o) PRG_PLATE=${OPTARG};;
+		\?) echo -e "\n Argument error! \n"; exit 0 ;;
+	esac
+done
+
+plate_f=${PLATE_NUMBER:0:2}
+plate_l=${PLATE_NUMBER:2:2}
+
+# check plate chars
+if [[ "${plate_f}" =~ [^A-Z] ]] ; then
+    echo -e "Param error: invalid first two chars of plate, use only [A-Z] \n"
+	exit 0
+fi
+
+if [[ "${plate_l}" =~ [^A-Z] ]] ; then
+    echo -e "Param error: invalid last two chars of plate, use only [A-Z] \n"
+	exit 0
+fi
+
+# check plate's length
+if [ ${#PLATE_NUMBER} != 4 ] ; then 
+    echo -e "Param error: length of PLATE_NUMBER must be 4 \n"
+    exit 0
+fi
+
+# vars delcaration
+current_timestamp=$(date +"%Y%m%d")
+OUT_DIR="OUT_DIR"
+params_gen_FILE="params_gen.xml"
+upper_bound=$(expr $PRG_PLATE + $n)
+
+if ! [ $upper_bound -lt 999 ] ; then 
+    echo -e "Param error: progressive plate number out of bounds (0 < PLATE_NUMBER < 999 - #iterations) \n"
+    exit 0
+fi
+
+# create OUT_DIR if not exist
+if ! [ -d $OUT_DIR ] ; then
+	mkdir $OUT_DIR
+	path_OUT_dir=$(realpath $OUT_DIR)
+    echo -e "create '$OUT_DIR' at path: '$path_OUT_dir' \n"
+    chmod 0777 "$path_OUT_dir"
+else
+	path_OUT_dir=$(realpath $OUT_DIR)
+fi
+
 
 # functions
 function generate_PAN 
@@ -28,7 +82,7 @@ function generate_PAN
     echo ${str}
 }
 
-function generate_PLATE
+function generate_PLATE_NUMBER
 {
     PRG=$1
     plate_f=$2
@@ -55,56 +109,6 @@ function convert_PLATE_to_HEX
 }
 
 
-while getopts n:f:l: flag
-do
-    	case "${flag}" in
-		n) n=${OPTARG};;
-        f) plate_f=${OPTARG};;
-        l) plate_l=${OPTARG};;
-		\?) echo -e "\n Argument error! \n"; exit 0 ;;
-	esac
-done
-
-# check plate chars
-if [[ "${plate_f}" =~ [^A-Z] ]] ; then
-    echo -e "Param error: invalid first two chars of plate, use only [A-Z] \n"
-	exit 0
-fi
-
-if [[ "${plate_l}" =~ [^A-Z] ]] ; then
-    echo -e "Param error: invalid last two chars of plate, use only [A-Z] \n"
-	exit 0
-fi
-
-# check plate's length
-if [[ ${#plate_f} != 2 ]]; 
-then
-    echo -e "Param error: invalid plate format (only two first chars, ex. AB) \n"
-    echo
-	exit 0
-fi
-
-if [[ ${#plate_l} != 2 ]]; 
-then
-    echo -e "Param error: invalid plate format (only two first chars, ex. AB) \n"
-    echo
-	exit 0
-fi
-
-# vars delcaration
-current_timestamp=$(date +"%Y%m%d")
-OUT_DIR="OUT_DIR"
-params_gen_FILE="params_gen.xml"
-
-# create OUT_DIR if not exist
-if ! [ -d $OUT_DIR ] ; then
-	mkdir $OUT_DIR
-	path_OUT_dir=$(realpath $OUT_DIR)
-    echo -e "create '$OUT_DIR' at path: '$path_OUT_dir' \n"
-    chmod 0777 "$path_OUT_dir"
-else
-	path_OUT_dir=$(realpath $OUT_DIR)
-fi
 
 # get LAST_PRG_USED
 if ! [ -f "$path_OUT_dir/$params_gen_FILE" ] ; then
@@ -118,11 +122,11 @@ if ! [ -f "$path_OUT_dir/$params_gen_FILE" ] ; then
         old_timestamp_value="${old_timestamp_value: 1:10}"
         old_timestamp_value="${old_timestamp_value//-}"
         
-        if [ $old_timestamp_value == $current_timestamp ] ; then
+        if  [ $old_timestamp_value == $current_timestamp ] ; then
             # get LAST PRG USED before tabula rasa
             line_last_PRG="$(grep 'LAST_PRG_USED:' "$path_OUT_dir/$params_gen_FILE")"
             last_PRG_value=${line_last_PRG#*:}
-            echo -e "file '$params_gen_FILE' found, the last prg used is $last_PRG_value \n"
+            echo -e "file '$params_gen_FILE' found, the last prg used is '$last_PRG_value' \n"
         else
             last_PRG_value=0
         fi 
@@ -131,9 +135,15 @@ if ! [ -f "$path_OUT_dir/$params_gen_FILE" ] ; then
 # tabula rasa of params_gen.xml
 echo > "$path_OUT_dir/$params_gen_FILE"
 
-MIN=$(expr $last_PRG_value + 1)
-MAX=$(expr $MIN + $n - 1)
-list_PRG=( $(seq $MIN $MAX) )
+if [ $PRG_PLATE != '' ] ; then
+    MIN=$PRG_PLATE
+    MAX=$(expr $MIN + $n)
+    list_PRG=( $(seq $MIN $MAX) )
+else 
+    MIN=$(expr $last_PRG_value + 1)
+    MAX=$(expr $MIN + $n - 1)
+    list_PRG=( $(seq $MIN $MAX) )
+fi
 
 elab_time=$(date +"%Y-%m-%d %H:%M:%S")
 
@@ -152,7 +162,7 @@ echo -e " --- PAN-TARGA-HEX triple n° $(expr $i + 1) --- OK \n"
 PRG=${list_PRG[i]}
 
 PAN=$(generate_PAN $PRG)
-PLATE=$(generate_PLATE $PRG $plate_f $plate_l)
+PLATE=$(generate_PLATE_NUMBER $PRG $plate_f $plate_l)
 HEX_PLATE=$(convert_PLATE_to_HEX $PLATE)
 
 #echo "$PAN - $PLATE - $HEX_PLATE"
